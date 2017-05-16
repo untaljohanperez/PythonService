b40ff94c96b175d9322f2c1b85b13d1c087dd077
Kedwin Johan Perez Zea <kedwpeze@suramericana.com.co>
2017-05-16 13:19:50 -0500

    add overload pcu endpoint

--- api.py
+++ api.py
@@ -2,10 +2,11 @@
 
 from flask import Flask
 from flask_restful import Resource, Api
+from datetime import datetime
+from datetime import timedelta
 
 app = Flask(__name__)
 api = Api(app)
-
 
 class Product(Resource):
     def get(self):
@@ -15,8 +16,21 @@ class Product(Resource):
                         'Morcilla',
                         'Fruit']
         }
+		
+class Overload(Resource):
+    def overLoad(self, seconds):
+        waitTill = datetime.now() + timedelta(seconds=seconds)
+        while (waitTill > datetime.now()):
+            print (seconds * seconds)
 
+    def get(self):
+        overLoad(500)
+        return { 
+            'time': datetime.now() 
+        }
+        
 api.add_resource(Product, '/')
+api.add_resource(Overload, '/overload')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050, debug=True)
